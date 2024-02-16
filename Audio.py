9ec2891a31af940638fffa3cee3d5287bf971146
yaelszn <yaelszn@post.bgu.ac.il>
2024-02-16 10:31:19 +0200

    changes in audio

--- Audio.py
+++ Audio.py
@@ -1,30 +1,30 @@
 import threading
 import wave
+import time
+import pygame.mixer
 
 import Settings as s
-import winsound
-from pygame import mixer
-import time
-
 
 class Audio(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        print ("AUDIO INITIALIZATION")
+        print("AUDIO INITIALIZATION")
 
     def run(self):
         while not s.finish_workout:
-            if s.str_to_say!="":
+            if s.str_to_say != "":
                 self.say_no_wait(s.str_to_say)
                 print("tts says: ", s.str_to_say)
                 s.str_to_say = ""
-        print ("AUDIO DONE")
+        print("AUDIO DONE")
 
     def say1(self, str_to_say):
-        if (str_to_say != ""):
-            winsound.PlaySound(s.audio_path+str_to_say+'.wav', winsound.SND_FILENAME)
-
+        if str_to_say != "":
+            pygame.mixer.music.load(s.audio_path + str_to_say + '.wav')
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
 
 def say(str_to_say):
     '''
@@ -32,16 +32,12 @@ def say(str_to_say):
     This function make the robot say whatever there is in the file - play the audio (paralelly)
     :return: audio
     '''
-    mixer.init()
-    mixer.music.load(s.audio_path+str_to_say+'.wav')
-    mixer.music.play()
-
-   # while mixer.music.get_busy():
-    #    time.sleep(0.1)
-
+    pygame.mixer.init()
+    pygame.mixer.music.load(s.audio_path + str_to_say + '.wav')
+    pygame.mixer.music.play()
 
 def get_wav_duration(str_to_say):
-    str_with_path= s.audio_path + str_to_say + '.wav'
+    str_with_path = s.audio_path + str_to_say + '.wav'
     with wave.open(str_with_path, 'rb') as wav_file:
         # Get the number of frames and the frame rate
         num_frames = wav_file.getnframes()
@@ -55,10 +51,13 @@ def get_wav_duration(str_to_say):
 if __name__ == '__main__':
     language = 'Hebrew'
     gender = 'Male'
-    s.audio_path = 'audio files/' + language + '/'+gender+'/'
+    s.audio_path = 'audio files/' + language + '/' + gender + '/'
 
-    # audio = Audio()
-    # audio.say('raise arms forward')
+    # Initialize pygame mixer
+    pygame.mixer.init()
+
+    # Use the say function to play audio
     say("calibration")
-    #time.sleep(5)
+    # Sleep for a while to allow the first audio to play
+    time.sleep(5)
     say("hello_waving")
